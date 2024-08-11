348506bbb0cfbad9ccc4f5d13354d7f546870231
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-08-11 22:28:21 +0400

    First Commit

---
+++ src/main/java/az/edu/turing/module1/lesson02/ScannerApp.java
@@ -0,0 +1,16 @@
+package az.edu.turing.module1.lesson02;
+
+import java.util.Scanner;
+
+public class ScannerApp {
+    public static void main(String[] args) {
+        {
+                System.out.println("Enter a number:");
+                Scanner scanner = new Scanner(System.in);
+                int number = scanner.nextInt();
+                String result = number % 2 == 0 ? "EVEN" : "ODD";
+                System.out.println(result);
+            }
+        }
+    }
+
---
+++ src/main/java/az/edu/turing/module2/lesson01/SwitchExample.java
@@ -0,0 +1,16 @@
+package az.edu.turing.module2.lesson01;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int a = 109;
+        switch (a) {
+            case 1 -> System.out.println("equals to 1");
+            case 50 -> System.out.println("equals to 50");
+            case 109 -> System.out.println("equals to 109");
+            case 200 -> System.out.println("equals to 200");
+            default -> System.out.println("Not found");
+        }
+
+    }
+}
+
