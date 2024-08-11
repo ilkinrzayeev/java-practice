927b0990127e26c7c3c3c31fb9cd6aade0659017
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-08-11 22:38:03 +0400

    Second Commit

---
+++ src/main/java/az/edu/turing/module2/lesson02/LettersScanApp.java
@@ -0,0 +1,12 @@
+package az.edu.turing.module2.lesson02;
+
+import java.util.Scanner;
+
+public class LettersScanApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        System.out.println(word);
+    }
+}
+
