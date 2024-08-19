bf2d8e6818302b5b3e9c29f9c6432791e2fea66b
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-08-19 22:18:11 +0400

    Commit

---
+++ src/main/java/az/edu/turing/module1/lesson03/StringLengthApp.java
@@ -0,0 +1,11 @@
+package az.edu.turing.module1.lesson03;
+
+import java.util.Scanner;
+
+public class StringLengthApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str.length() * 2);
+    }
+}
