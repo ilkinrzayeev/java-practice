6243b7016b43c8092a39e08d5883cc57ad0fff66
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-08-22 13:07:11 +0400

    Alphabet

---
+++ src/main/java/az/edu/turing/module1/lesson03/AlphabetApp.java
@@ -0,0 +1,12 @@
+package az.edu.turing.module1.lesson03;
+
+public class AlphabetApp {
+
+    public static void main(String[] args) {
+        for (int i = 122; i >= 97; i--) {
+            System.out.println((char) i);
+        }
+    }
+
+}
+
