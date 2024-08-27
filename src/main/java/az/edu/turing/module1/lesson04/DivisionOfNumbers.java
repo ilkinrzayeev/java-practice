8c7d098a344283dbfa907c7068aae9bbc07b767e
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-08-27 15:49:42 +0400

    Divisions of Numbers

---
+++ src/main/java/az/edu/turing/module1/lesson04/DivisionOfNumbers.java
@@ -0,0 +1,21 @@
+package az.edu.turing.module1.lesson04;
+
+import java.util.Scanner;
+
+public class DivisionOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Divisors of" + ": " + number);
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
