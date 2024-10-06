67c101cb00bab8347d60ad5d18860bc0ae1207ce
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-10-07 02:46:22 +0400

    Do While Loop

---
+++ src/main/java/az/edu/turing/module1/lesson03/DoWhileApp.java
@@ -0,0 +1,11 @@
+package az.edu.turing.module1.lesson03;
+
+public class DoWhileApp {
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("Hi there");
+        } while (7 < 6);
+    }
+}
+
