e61bc55442a70fb7c6f4fdf3406b1c7a4519288c
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-10-06 23:29:22 +0400

    Addition and multiplication operations

---
+++ src/main/java/az/edu/turing/module1/lesson04/NumberApp.java
@@ -0,0 +1,22 @@
+package az.edu.turing.module1.lesson04;
+
+import java.util.Scanner;
+
+public class NumberApp {
+
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+
+        int sum = 0;
+        long prod = 1;
+        while (num > 0) {
+            int remainder = num % 10;
+            sum += remainder;
+            prod *= remainder;
+            num /= 10;
+        }
+
+        System.out.println(sum);
+        System.out.println(prod);
+    }
+}
